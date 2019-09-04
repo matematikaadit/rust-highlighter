19fc1c0243a3cd09cee39148a9936336bda7c427
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2019-09-04 15:17:10 +0700

    Testing the rustc_lexer api by using it to lex this project source code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use rustc_lexer::TokenKind;
+
 fn main() {
-    println!("Hello, world!");
+    let source = include_str!("main.rs");
+    let mut rest = source;
+    for token in rustc_lexer::tokenize(source) {
+        match token.kind {
+            TokenKind::Whitespace => {},
+            _ => {
+                println!("str: {}, kind: {:?}", &rest[..token.len], token.kind);
+            }
+        }
+        rest = &rest[token.len..]
+    }
 }
